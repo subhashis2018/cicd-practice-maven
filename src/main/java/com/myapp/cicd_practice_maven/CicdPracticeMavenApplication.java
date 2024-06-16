109f321d95f157158a7424159a8470782fb2d10c
SUBHASHIS <subhashis.sarangi@gmail.com>
2024-06-16 08:00:06 +0530

    committing skeleton.

---
+++ src/main/java/com/myapp/cicd_practice_maven/CicdPracticeMavenApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.cicd_practice_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPracticeMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdPracticeMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/cicd_practice_maven/CicdPracticeMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.cicd_practice_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPracticeMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
